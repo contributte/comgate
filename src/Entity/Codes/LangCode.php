f8ea5afd8f5b60826b84c10092b44c6068b6c865
Radim Hašek <radim.hasek@gmial.com>
2020-01-13 00:46:28 +0100

    fix codesniffer errors

--- src/Entity/Codes/LangCode.php
+++ src/Entity/Codes/LangCode.php
@@ -2,15 +2,12 @@
 
 namespace Contributte\Comgate\Entity\Codes;
 
-/**
- * Supported lang code by ISO 639-1
- */
 final class LangCode
 {
 
-    public const CS = 'cs';
-    public const SK = 'sk';
-    public const EN = 'en';
-    public const PL = 'pl';
-    
+	public const CS = 'cs';
+	public const SK = 'sk';
+	public const EN = 'en';
+	public const PL = 'PL';
+
 }
