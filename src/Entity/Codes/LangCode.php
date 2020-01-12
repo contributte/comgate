55f8b222317a69feafdf11effd4149cb8ccbaaf5
Radim Hašek <radim.hasek@gmial.com>
2020-01-12 22:30:33 +0100

    fix codesniffer errors

--- src/Entity/Codes/LangCode.php
+++ src/Entity/Codes/LangCode.php
@@ -7,8 +7,10 @@ namespace Contributte\Comgate\Entity\Codes;
  */
 final class LangCode
 {
+
 	public const CS = 'cs';
 	public const SK = 'sk';
 	public const EN = 'en';
 	public const PL = 'pl';
+	
 }
