aeb2f85072f47af058d03fc01349ab4d8170aad5
Radim Hašek <radim.hasek@gmail.com>
2020-01-13 15:47:47 +0100

    Feature: payments outside the Czech Republic (#5)

---
+++ src/Entity/Codes/LangCode.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Comgate\Entity\Codes;
+
+final class LangCode
+{
+
+	public const CS = 'cs';
+	public const SK = 'sk';
+	public const EN = 'en';
+	public const PL = 'PL';
+
+}
